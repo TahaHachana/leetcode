5ab58d7ddacbd3ae34213efde206d1de38d4af54
TahaHachana <tahahachana@gmail.com>
2024-08-17 15:59:36 +0100

    344 reverse string

---
+++ src/344.py
@@ -0,0 +1,15 @@
+# Reverse String
+# https://leetcode.com/problems/reverse-string/description/
+
+from typing import List
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
